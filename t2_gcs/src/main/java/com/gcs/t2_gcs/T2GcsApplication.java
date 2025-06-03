5b74c0b2882ee8a39eb2ab25a57c29f5d346e1ea
Mateus Neubarth <mateusneubarth@hotmail.com>
2025-06-03 11:33:39 -0300

    initial commit

---
+++ t2_gcs/src/main/java/com/gcs/t2_gcs/T2GcsApplication.java
@@ -0,0 +1,13 @@
+package com.gcs.t2_gcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2GcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T2GcsApplication.class, args);
+	}
+
+}
---
+++ t2_gcs/src/test/java/com/gcs/t2_gcs/T2GcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcs.t2_gcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T2GcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
